f6b45191b8b45d0204dd60e2bf30557d8f3f6953
Bossun <Kazehaya.ex@gmail.com>
2016-11-13 20:53:48 -0300

    Agregado jquery

---
+++ Lista.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista .-</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-primary">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Biblioteca</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Favoritos</a></li>
+                    <li><a href="#">Mi lista</a></li>
+                    <li><a href="#">Mi cuenta</a></li>
+
+                    <form class="navbar-form navbar-left" method="GET" action="#">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Anime">
+                            <button type="submit" class="btn btn-default">Buscar</button>
+                        </div>
+                    </form>
+            </div>
+        </nav>
+    </header>
+    <main>
+        
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="./js/jquery-3.1.1.min.js "></script>
+    <script src="./js/bootstrap.js "></script>
+
+</body>
+
+</html>
